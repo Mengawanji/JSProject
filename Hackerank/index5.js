6964161953fe2efd9b409838681dc8351e44f5e4
Menga wanji <martialwanji@gmail.com>
2025-10-13 20:37:29 +0100

    solved challenges on hackerank

---
+++ Hackerank/index5.js
@@ -0,0 +1,42 @@
+// Task
+
+// Complete the getGrade(score) function in the editor. It has one parameter: 
+// an integer,score, denoting the number of points Julia earned on an exam. It must 
+// return the letter corresponding to her grade according to the  rules:
+
+
+function getGrade(score) {
+    let grade;
+    // Write your code here
+    
+    if (0<= score  && score <= 5 ) {
+        grade = 'F';
+    }else if (5< score  && score <= 10 ) {
+        grade = 'E';
+    }   else if (10< score && score <= 15 ) {
+        grade = 'D';
+    } else if (15< score && score <= 20 ) {
+        grade = 'C';
+    }else if (20< score && score <= 25 ) {
+        grade = 'B';
+    }else if (25< score && score <= 30 ) {
+        grade = 'A';
+    }
+    
+    
+    return grade;
+}
+
+
+
+//===========================
+
+function getCount(objects) {
+        let count = 0;
+    for (let obj of objects) {
+        if (obj.x === obj.y) {
+            count++;
+        }
+    }
+    return count;
+}
