e8862dfd351f8d98895bb7f9caca81d130cfd596
Menga wanji <martialwanji@gmail.com>
2025-10-17 23:11:40 +0100

    solved hackerank challenges

--- Hackerank/index5.js
+++ Hackerank/index5.js
@@ -40,3 +40,89 @@ function getCount(objects) {
     }
     return count;
 }
+
+
+//Find the second largest number in an arrey
+
+
+function getSecondLargest(nums) {
+    
+    const largest = Math.max(...nums);
+    
+    const withoutLargest = nums.filter(num => num < largest); 
+    
+    if(withoutLargest === 0){
+        return largest
+    }  
+
+    return Math.max(...withoutLargest)
+}
+
+
+
+
+// Switch Cases :
+
+// Complete the getLetter(s) function in the editor. 
+// It has one parameter: a string, , consisting of lowercase English 
+// alphabetic letters (i.e., a through z). 
+// It must return A, B, C, or D depending on the following criteria:
+
+// {a, e, i, o, u} → 'A'
+// {b, c, d, f, g} → 'B'
+// {h, j, k, l, m} → 'C'
+// {n, p, q, r, s, t, v, w, x, y, z} → 'D'
+
+
+function getLetter(s) {
+    let letter;
+    switch(true){
+        case "a e i o u".includes(s[0]):
+        letter = 'A';
+        break
+        
+        case "b c d f g".includes(s[0]):
+        letter = 'B';
+        break;
+        
+        case "h j k l m".includes(s[0]):
+        letter = 'C';
+        break;
+        
+        case "n p q r s t v w x y z".includes(s[0]):
+        letter = 'D';
+        break;
+    }
+
+    return letter;
+}
+
+// Task
+
+// Create a Polygon class that has the following properties:
+
+// A constructor that takes an array of integer values describing the lengths of the polygon's sides.
+// A perimeter() method that returns the polygon's perimeter.
+// Locked code in the editor tests the Polygon constructor and the perimeter method.
+
+// Note: The perimeter method must be lowercase and spelled correctly.
+
+// Input Format
+
+
+// Define the Polygon class
+class Polygon {
+  constructor(sides) {
+    this.sides = sides; // store array of side lengths
+  }
+
+  // method to return the perimeter
+  perimeter() {
+    return this.sides.reduce((a, b) => a + b, 0);
+  }
+}
+
+// Example:
+let triangle = new Polygon([3, 4, 5]);
+console.log(triangle.perimeter()); // Output: 12
+
